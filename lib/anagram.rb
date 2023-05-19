77902d156ab3a7f54965a41afeb973a949d0d7c5
Owallah <mikesonowallah@gmail.com>
2023-05-19 10:01:09 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word = 'listen')
+        @word = word
+    end
+
+    def match(word)
+        anagram = []
+        isAnagram = false
+        word.map do |arr_word|
+            test_word = arr_word.chars
+            isAnagram_arr = test_word.sort == @word.chars.sort
+            if isAnagram_arr == true
+                isAnagram = true
+                anagram.push(test_word.join(''))
+            end
+        end
+        anagram
+        
+    end
+end
+
+diaper = Anagram.new('diaper')
+diaper.match(%w[hello world zombies pants dipper])
